4c98219ea30650fbe85d4c363674bdd8167f07f2
arman <terminator_arman@yahoo.com>
2024-06-19 22:30:30 +0330

    Implemented singleton pattern.

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Singleton
+﻿namespace Singleton
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            SingletonClass singleton = SingletonClass.Instance;
+
         }
     }
 }
---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,35 @@
+﻿namespace Singleton
+{
+    public class SingletonClass
+    {
+        private SingletonClass() { }
+
+        private static SingletonClass instance;
+
+        /// <summary>
+        /// This property creates an instance for you if doesn't exists any instance of this class and returns it.
+        /// otherwise, returns previous instance.
+        /// </summary>
+        public static SingletonClass Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonClass();
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// The sample value with int type.
+        /// </summary>
+        public int intValue { get; set; }
+
+        /// <summary>
+        /// The sample value with string type.
+        /// </summary>
+        public string strValue { get; set; }
+
+    }
+}
